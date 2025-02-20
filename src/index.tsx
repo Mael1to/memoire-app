a851a112851aaa7427f7683f94f5a5665836c719
Erion-pdz <parduzi.erion@hotmail.com>
2025-02-20 16:00:40 +0100

    mode hors ligne

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,17 @@ root.render(
     <App />
   </React.StrictMode>
 );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker enregistré avec succès:', registration);
+      })
+      .catch((error) => {
+        console.log('Erreur lors de l’enregistrement du Service Worker:', error);
+      });
+  });
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
